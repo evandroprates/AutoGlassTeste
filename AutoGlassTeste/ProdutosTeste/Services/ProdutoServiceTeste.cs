dc0252cabc7c2b7b417b088eb2fbf2f921eadc66
evandroprates <evandroprates15@gmail.com>
2022-05-11 10:53:40 -0300

    Corrigindo camada de teste

--- AutoGlassTeste/ProdutosTeste/Services/ProdutoServiceTeste.cs
+++ AutoGlassTeste/ProdutosTeste/Services/ProdutoServiceTeste.cs
@@ -1,8 +1,10 @@
 ﻿using AutoGlassTeste.Application;
 using AutoGlassTeste.Application.DTO;
 using AutoGlassTeste.Application.Interfaces;
+using AutoGlassTeste.Domain.Core.Interfaces.Repository;
 using AutoGlassTeste.Domain.Core.Interfaces.Services;
 using AutoGlassTeste.Domain.Entity;
+using AutoMapper;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -15,26 +17,26 @@ namespace ProdutosTeste.Services
 {
     public class ProdutoServiceTeste
     {
-        //private ApplicationServiceProduto produtoService;
+        private ApplicationServiceProduto produtoService;
 
-        //public ProdutoServiceTeste()
-        //{
-        //    produtoService = new ApplicationServiceProduto(new Mock<IServiceProduto>().Object, new Mock<IMapperProduto>().Object);
-        //}
+        public ProdutoServiceTeste()
+        {
+            produtoService = new ApplicationServiceProduto(new Mock<IServiceProduto>().Object, new Mock<IRepositoryProduto>().Object, new Mock<IMapper>().Object);
+        }
 
-        //[Fact]
-        //public void Post_ValidandoAtualizacao()
-        //{
-        //    var exception = Assert.Throws<Exception>(() => produtoService.Update(new Produto()));
-        //    Assert.Equal("ID não é válido", exception.Message);
-        //}
+        [Fact]
+        public void Post_ValidandoAtualizacao()
+        {
+            var exception = Assert.Throws<Exception>(() => produtoService.Update(new Produto()));
+            Assert.Equal("ID não é válido", exception.Message);
+        }
 
-        //[Fact]
-        //public void Post_ValidandoDelete()
-        //{
-        //    var exception = Assert.Throws<Exception>(() => produtoService.Remove(new Produto()));
-        //    Assert.Equal("ID não é válido", exception.Message);
-        //}
+        [Fact]
+        public void Post_ValidandoDelete()
+        {
+            var exception = Assert.Throws<Exception>(() => produtoService.Remove(new Produto()));
+            Assert.Equal("ID não é válido", exception.Message);
+        }
 
     }
 }
